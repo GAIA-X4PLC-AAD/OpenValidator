7cc3c26a9ed2a34d6f4e37b1ec048ad3e6022e7f
Hadi Adineh <hadi.adineh@asc-s.de>
2023-12-08 11:06:10 +0100

    validator as github action test 7 - exit code 1 on entrypoint (#1)
    Signed-off-by: Hadi Adineh <hadi.adineh@asc-s.de>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 
-# file=$(find data/inputs -type f -name "0" -print -quit)
+file=$(find data/inputs -type f -name "0" -print -quit)
 
-# cp "$file" data/inputs
+cp "$file" data/inputs
 
-# python3 main.py "$file"
 
-# cp -r /app/reports data/outputs
+python3 main.py "$file"
 
-# python main.py data/inputs/0 -o data/outputs/
-echo $1
-echo $2
-python main.py /app/$1 -o /app/$2
+cp -r /app/reports data/outputs
--- entrypoint_GitHubAction_Open_Validator.sh
+++ entrypoint_GitHubAction_Open_Validator.sh
@@ -1,18 +1,21 @@
-#!/bin/bash
-
-
-# file=$(find data/inputs -type f -name "0" -print -quit)
-
-# cp "$file" data/inputs
-
-# python3 main.py "$file"
-
-# cp -r /app/reports data/outputs
-
-# python main.py data/inputs/0 -o data/outputs/
 echo $1
 echo $2
-python main.py /app/$1 -o /app/$2
-
-time=$(date)
-echo "time=$time, output=/data" >> $GITHUB_OUTPUT
+python main.py /app/$1 -o "/app/$2"
+echo " EXIT CODE : $?"
+echo "param 1 : ($1)"
+echo "param 2 : ($2)"
+exit 1
+# exit code
+# if [ $? -eq 0 ]; then
+#     echo "success"
+#     echo "::set-output name=exit_code::0"
+#     # time=$(date)
+#     # echo "time=$time, output=/data, sucess" >> $GITHUB_OUTPUT
+#     # exit 0
+# else
+#     echo "failure"
+#     echo "::set-output name=exit_code::1"
+#     # time=$(date)
+#     # echo "time=$time, output=/data, failure" >> $GITHUB_OUTPUT
+#     # exit 1
+# fi
