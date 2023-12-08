34ba3d2b4cad4d1c70b25f1221af8c0f1ce37745
Hadi Adineh <hadi.adineh@asc-s.de>
2023-12-08 11:39:44 +0100

    validator as github action test 12 (#1)
    Signed-off-by: Hadi Adineh <hadi.adineh@asc-s.de>

--- entrypoint_GitHubAction_Open_Validator.sh
+++ entrypoint_GitHubAction_Open_Validator.sh
@@ -1,27 +1,16 @@
 result=$(python main.py /app/$1 -o "/app/$2")
 exit_code=$?
+time=$(date)
 echo "result : $result"
 echo "EXIT CODE : $exit_code"
 echo "param 1 : ($1)"
 echo "param 2 : ($2)"
-if [ $exit_code -eq 1 ]; then
+if [ $exit_code -eq 0 ]; then
     echo "success"
+    echo "time=$time, output=/data, sucess" >> $GITHUB_OUTPUT
     exit 0
 else
     echo "failure"
+    echo "time=$time, output=/data, failure" >> $GITHUB_OUTPUT
     exit 1
 fi
-# exit code
-# if [ $? -eq 0 ]; then
-#     echo "success"
-#     echo "::set-output name=exit_code::0"
-#     # time=$(date)
-#     # echo "time=$time, output=/data, sucess" >> $GITHUB_OUTPUT
-#     # exit 0
-# else
-#     echo "failure"
-#     echo "::set-output name=exit_code::1"
-#     # time=$(date)
-#     # echo "time=$time, output=/data, failure" >> $GITHUB_OUTPUT
-#     # exit 1
-# fi
