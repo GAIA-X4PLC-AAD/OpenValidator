5732431129a19752c72f48cd56e3cd4da8a99fb4
Hadi Adineh <hadi.adineh@asc-s.de>
2023-12-08 12:01:23 +0100

    validator as github action test 16 - json result (#1)
    Signed-off-by: Hadi Adineh <hadi.adineh@asc-s.de>

--- entrypoint_GitHubAction_Open_Validator.sh
+++ entrypoint_GitHubAction_Open_Validator.sh
@@ -1,16 +1,16 @@
-result=$(python main.py /app/$1 -o "/app/$2")
+returns=$(python main.py /app/$1 -o "/app/$2")
 exit_code=$?
 time=$(date)
-echo "result : $result"
+echo "returns : $returns"
 echo "EXIT CODE : $exit_code"
 echo "param 1 : ($1)"
 echo "param 2 : ($2)"
 if [ $exit_code -eq 0 ]; then
     echo "success"
-    echo "validation_result={"time":"$time", "output":"$2", "validation":"sucess"" >> $GITHUB_OUTPUT
+    echo "validation_result={"time":"$time", "output":"$2", "returns":"$returns", "validation":"sucess"}" >> $GITHUB_OUTPUT
     exit 0
 else
     echo "failure"
-    echo "validation_result={"time":"$time", "output":"$2", "validation":"failure"" >> $GITHUB_OUTPUT
+    echo "validation_result={"time":"$time", "output":"$2", "returns":"$returns", "validation":"failure"}" >> $GITHUB_OUTPUT
     exit 1
 fi
